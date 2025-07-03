3ab5afaabe15bf27ab6001c877d355592d49b4e0
PeterLe611 <duclocle.work@gmail.com>
2025-07-03 21:48:00 +0700

    Init project's environment and key dependencies

---
+++ backend/src/server.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+const app = express();
+const PORT = process.env.PORT || 3001
+
+// Middleware
+app.use(cors(
+    {
+        origin: "http://localhost:5147", // Adjust this to your frontend URL
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
+    }
+));
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+    res.send("Welcome to the PlanBro Event Planning API!");
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
